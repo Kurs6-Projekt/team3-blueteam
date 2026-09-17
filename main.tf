b0bb05c39a97825368cf11646bedb365909a3d11
jonatsub <255294695+Jonatsub@users.noreply.github.com>
2026-09-17 15:21:59 +0200

    Ta bort ssh-keys ur jumphostens metadata
    Enligt steg 3 ska nycklarna bort när OS Login funkar. Alla i gruppen har
    loggat in med sina OS Login-namn nu, så det är uppfyllt.
    
    Nycklarna låg i klartext i metadatan. Alla som hade ett konto på maskinen
    kunde läsa dem, man behövde inte ens sudo.
    
    Jag har inte rört primary. Den kör fortfarande på metadata-nycklar och har
    inga IAM-bindningar för OS Login, så tar man bort nycklarna där kommer man
    inte in alls. Skriver ett separat fynd på det.

--- main.tf
+++ main.tf
@@ -118,7 +118,6 @@ resource "google_compute_instance" "jumphost" {
 
   metadata = {
     enable-oslogin         = "TRUE"
-    ssh-keys               = join("\n", [for user in var.ssh_users : "${user.username}:${user.public_key}"])
     block-project-ssh-keys = true
     startup-script         = <<-EOT
       #!/bin/bash
