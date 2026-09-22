d2a91fba13d2a342622e0f230a2b7592083bfd25
andreedvardsson <254701176+andreedvardsson@users.noreply.github.com>
2026-09-22 15:42:39 +0200

    feat(network): enable VPC flow logs

--- main.tf
+++ main.tf
@@ -29,10 +29,17 @@ data "google_compute_network" "team_vpc" {
 }
 
 resource "google_compute_subnetwork" "team" {
-  name          = "team${var.team_id}-subnet"
-  ip_cidr_range = local.subnet_cidr
-  region        = var.region
-  network       = data.google_compute_network.team_vpc.id
+  name                     = "team${var.team_id}-subnet"
+  ip_cidr_range            = local.subnet_cidr
+  region                   = var.region
+  network                  = data.google_compute_network.team_vpc.id
+  private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
 
 resource "google_compute_address" "jumphost" {
